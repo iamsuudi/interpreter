31c2380ae9d1dca7a9ffd39d810f2a60cfef0ed3
Abdulfetah <suudiabdulfetah@gmail.com>
2024-10-27 17:46:34 +0300

    feat: Expand the tokens set and update the lexer

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -1,23 +1,98 @@
 package lexer
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/iamsuudi/interpreter/token"
 )
 
 func TestNextToken(t *testing.T) {
-	input := `=+(){}`
+	input := `
+		let math = 95;
+		let phy = 94;
+		let add = fn(x, y) {
+			x + y;
+		};
+		let result = add(math, phy);
+		!-/*5;
+		5 < 10 > 5;
+
+		if (5 == 10) {
+			return true;
+		} else {
+			return false;
+		}
+	`
 
 	tests := []struct {
 		expectedType    token.TokenType
 		expectedLiteral string
 	}{
+		{token.LET, "let"},
+		{token.IDENTIFIER, "math"},
 		{token.ASSIGN, "="},
-		{token.PLUS, "+"},
+		{token.INT, "95"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENTIFIER, "phy"},
+		{token.ASSIGN, "="},
+		{token.INT, "94"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENTIFIER, "add"},
+		{token.ASSIGN, "="},
+		{token.FUNCTION, "fn"},
 		{token.LPAREN, "("},
+		{token.IDENTIFIER, "x"},
+		{token.COMMA, ","},
+		{token.IDENTIFIER, "y"},
 		{token.RPAREN, ")"},
 		{token.LBRACE, "{"},
+		{token.IDENTIFIER, "x"},
+		{token.PLUS, "+"},
+		{token.IDENTIFIER, "y"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENTIFIER, "result"},
+		{token.ASSIGN, "="},
+		{token.IDENTIFIER, "add"},
+		{token.LPAREN, "("},
+		{token.IDENTIFIER, "math"},
+		{token.COMMA, ","},
+		{token.IDENTIFIER, "phy"},
+		{token.RPAREN, ")"},
+		{token.SEMICOLON, ";"},
+		{token.BANG, "!"},
+		{token.MINUS, "-"},
+		{token.SLASH, "/"},
+		{token.ASTERISK, "*"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.GT, ">"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.IF, "if"},
+		{token.LPAREN, "("},
+		{token.INT, "5"},
+		{token.EQ, "=="},
+		{token.INT, "10"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.TRUE, "true"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.ELSE, "else"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.FALSE, "false"},
+		{token.SEMICOLON, ";"},
 		{token.RBRACE, "}"},
 		{token.EOF, ""},
 	}
@@ -26,6 +101,8 @@ func TestNextToken(t *testing.T) {
 
 	for i, tt := range tests {
 		tok := l.NextToken()
+
+		fmt.Println(tok)
 
 		if tok.Type != tt.expectedType {
 			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q", i, tt.expectedType, tok.Type)
--- token/token.go
+++ token/token.go
@@ -16,8 +16,19 @@ const (
 	INT        = "INT"
 
 	// Operators
-	ASSIGN = "="
-	PLUS   = "+"
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT  = "<"
+	LTE = "<="
+	GT  = ">"
+	GTE = ">="
+	EQ = "=="
+	NEQ = "!="
 
 	// Delimiters
 	COMMA     = ","
@@ -31,4 +42,26 @@ const (
 	// Keywords
 	FUNCTION = "fn"
 	LET      = "let"
+	TRUE     = "true"
+	FALSE    = "false"
+	IF       = "if"
+	ELSE     = "else"
+	RETURN   = "return"
 )
+
+var Keywords = map[string]TokenType{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+}
+
+func LookupIndentifier(indentifier string) TokenType {
+	if tok, ok := Keywords[indentifier]; ok {
+		return tok
+	}
+	return IDENTIFIER
+}
